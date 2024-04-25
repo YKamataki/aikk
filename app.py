964db4c0fb6a32cb4b39098dbf14f10b7d141608
KAMATAKI Yuya <k212302@cc.k.tsukuba-tech.ac.jp>
2024-04-25 15:31:06 +0900

    Update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import curses
 from roma2kana import romaji_to_kana_convert
 
 def main(stdscr):
-    # curses.curs_set(0)  # カーソル非表示
+    curses.curs_set(0)  # カーソル非表示
     stdscr.nodelay(True)  # 非ブロッキングモード
     input_str = ''
     display_str = ''
@@ -17,6 +17,10 @@ def main(stdscr):
             key == ord('c') or \
             key == ord('v'):
             break
+        if key == ord("\n"):  # Enterで変換
+            stdscr.clear()
+            stdscr.addstr(0, 0, "> " + display_str + input_str)  # 結合された文字を表示
+            stdscr.addstr(1, 0, "Not Implemented\n")
         elif key != -1:
             char = chr(key)
             if char.isalpha():  # アルファベットのみ処理
@@ -25,7 +29,7 @@ def main(stdscr):
                 if converted:
                     display_str += converted
             stdscr.clear()
-            stdscr.addstr(0, 0, "Combined: " + display_str + input_str)  # 結合された文字を表示
+            stdscr.addstr(0, 0, "> " + display_str + input_str)  # 結合された文字を表示
 
 if __name__ == '__main__':
     curses.wrapper(main)
