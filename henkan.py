f93bdd9bbb4186702543678c0ea44f0c765bd1dc
KAMATAKI Yuya <k212302@cc.k.tsukuba-tech.ac.jp>
2024-04-25 17:18:24 +0900

    impled henkan()

--- henkan.py
+++ henkan.py
@@ -1,7 +1,14 @@
 from openai import OpenAI
 
-def henkan(ipt:str, c) -> str:
+def henkan(ipt:str, client) -> str:
     # get response
-    # messages = c.chat
-    return ipt + "(変換)"
+    res = client.chat.completions.create(
+            model='gpt-3.5-turbo',
+            messages=[
+                {'role': 'system', 'content': 'あなたはかな漢字変換器です。変換結果のみを出力すること。'},
+                {'role': 'system', 'content': '入力された文章をかな漢字変換してください。'},
+                {'role': 'user', 'content': ipt}
+                ]
+            )
+    return res.choices[0].message
 
