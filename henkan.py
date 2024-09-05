e8c669240a246760cf2a73805eb0f3c2471cb2b7
Yuya <k212302@cc.k.tsukuba-tech.ac.jp>
2024-09-05 13:14:35 +0900

    Change model

--- henkan.py
+++ henkan.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 def henkan(ipt:str, client) -> str:
     # get response
     res = client.chat.completions.create(
-            model='gpt-3.5-turbo',
+            model='gpt-4o-mini',
             messages=[
                 {'role': 'system', 'content': 'あなたはかな漢字変換器です。変換結果のみを出力すること。'},
                 {'role': 'system', 'content': '入力された文章をかな漢字変換してください。'},
